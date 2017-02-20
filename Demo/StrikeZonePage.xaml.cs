38228ffa987c9bc47bed5001afa01f3c4cdb59d3
Brock Billings <brock_billings@me.com>
2017-02-20 15:19:10 -0600

    Created the strike zone poage and view

---
+++ Demo/StrikeZonePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Demo
+{
+	public partial class StrikeZonePage : ContentPage
+	{
+		public StrikeZonePage()
+		{
+			InitializeComponent();
+
+			Content = CreateContent();
+		}
+
+		private View CreateContent()
+		{
+			var statusLabel = new Label
+			{
+				Text = "It was a strike...",
+				TextColor = Color.Black,
+				HorizontalTextAlignment = TextAlignment.Center
+			};
+
+			var stackLayout = new StackLayout
+			{
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			stackLayout.Children.Add(new StrikeZoneView());
+			stackLayout.Children.Add(statusLabel);
+
+			return stackLayout;
+		}
+	}
+}
